1ef57470bc52afad15697dc78e9e6b663f82a03e
James <2901470+JimShady@users.noreply.github.com>
2022-05-09 22:19:31 +0100

    Update remediationbot-tweet.R

--- remediationbot-tweet.R
+++ remediationbot-tweet.R
@@ -4,7 +4,8 @@ remediationbot_token <- rtweet::create_token(
   consumer_key =    Sys.getenv("TWITTER_API_KEY"),
   consumer_secret = Sys.getenv("TWITTER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
 )
 
 
