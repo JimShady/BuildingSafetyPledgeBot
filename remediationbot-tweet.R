830bb45639ebf3bc57d080b3327b594cb58dbc14
James <2901470+JimShady@users.noreply.github.com>
2022-05-09 22:39:50 +0100

    Update remediationbot-tweet.R

--- remediationbot-tweet.R
+++ remediationbot-tweet.R
@@ -2,12 +2,11 @@
 remediationbot_token <- rtweet::create_token(
   app = "BuildingSafetyPledgeBot",
   consumer_key =    Sys.getenv("TWITTER_API_KEY"),
-  consumer_secret = Sys.getenv("TWITTER_API_SECRET"),
+  consumer_secret = Sys.getenv("TWITTER_API_KEY_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
   set_renv = FALSE
 )
-
 
 # Post the image to Twitter
 rtweet::post_tweet(
