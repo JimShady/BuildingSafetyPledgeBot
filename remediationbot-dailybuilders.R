0a95e6fcb1b00a49dbe6aee5d4782959377fae88
James David Smith <james.david.smith@gmail.com>
2022-05-13 21:58:28 +0100

    small updates

--- remediationbot-dailybuilders.R
+++ remediationbot-dailybuilders.R
@@ -2,6 +2,7 @@ library("rtweet")
 library("readr")
 library("lubridate")
 library("glue")
+library("dplyr")
 
 # Create Twitter token
 remediationbot_token <- create_token(
@@ -13,22 +14,19 @@ remediationbot_token <- create_token(
   set_renv = FALSE
 )
 
-companies_yet_to_sign <- read_csv("companies_yet_to_sign.csv", col_types = cols())
+builders_yet_to_sign <- read_csv("builders_yet_to_sign.csv", col_types = cols())
 
 days_since <- as.Date("2022-04-12", format = "%Y-%m-%d")
 
-for (i in 1:nrow(companies_yet_to_sign)) {
+i <- sample(1:nrow(builders_yet_to_sign),1)
 
-  days <- today() - days_since
-  companies <- companies_yet_to_sign[i,]$twitter_handles
+days <- today() - days_since
+
+builders <- builders_yet_to_sign[i,]$twitter_handles
 
 #Post the image to Twitter
 post_tweet(
-  status = glue("It has been {days} days since the UK's largest homebuilders agreed to pay for the remediation of the defective buildings they built. Why are {companies} not on the list? #endourcladdingscandal #buildingsafetycrisis https://www.gov.uk/guidance/list-of-developers-who-have-signed-building-safety-repairs-pledge"), # nolint
+  status = glue("It has been {days} days since the UK's largest homebuilders agreed to pay to fix the defective buildings they built. Why are {builders} not on the list? #endourcladdingscandal #buildingsafetycrisis https://www.gov.uk/guidance/list-of-developers-who-have-signed-building-safety-repairs-pledge"), # nolint
   #media = temp_file,
   token = remediationbot_token
 )
-
-Sys.sleep(10)
-
-}
